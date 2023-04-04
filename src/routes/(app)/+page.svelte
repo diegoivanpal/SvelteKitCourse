fdaa3eb501e5cf3c2bdd1e44dd678f3575697ab2
diegoivanpal <58339715+diegoivanpal@users.noreply.github.com>
2023-04-04 14:28:19 -0300

    Primer commit curso SveleKit

---
+++ src/routes/(app)/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    
+</script> 
+
+
+<div class="index">
+    <h2>Welcome</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolores aut magni esse velit culpa sequi nisi ducimus? Provident magnam nobis rem expedita placeat, ullam, perferendis non, temporibus cupiditate accusamus dicta.</p>
+    <!-- <button on:click={updateTitle}>change title</button>
+    <input type="text" bind:value={title}> -->
+    <a href="/guides" >View Guides</a>
+    <a href="/about">About</a>
+</div>
+
+<style>
+    .index {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+    }
+</style>
---
+++ src/routes/(app)/about/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    
+</script> 
+
+
+<div class="about">
+    <h2>About us</h2>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam magnam rerum quod similique ullam delectus nobis, odit molestiae nemo molestias commodi veritatis sunt dicta dignissimos. Sed hic vitae libero eum?</p>
+    <a href="/guides" >View Guides</a>
+    <a href="/">Home</a>
+</div>
+
+<style>
+    .about {
+        text-align: center;
+        display: block;
+        margin: 20px auto;
+
+    }
+</style>
